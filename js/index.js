6100b4973bd9a669329eabac9c6f9f32bbe67974
General_D <joewebkid@yandex.ru>
2020-11-27 07:33:53 -0500

    r 80

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 3,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  })
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
